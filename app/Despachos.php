3c106d1c952f21e00919becfa43ef6f2a76beba3
Cristian Alejandro Gómez <crisoftpc@yahoo.com>
2018-01-13 00:06:52 -0600

    integracion de esquema de check list

---
+++ app/Despachos.php
@@ -0,0 +1,79 @@
+<STYLE TYPE="text/css" >
+
+input[type=checkbox], input[type=radio] {
+    box-sizing: border-box;
+    padding: 0;
+    -webkit-transform: scale(2);
+}
+
+.input-group {
+     margin-top: 12px;
+}
+
+div[class*='col-'] {
+    margin-bottom: 2px;
+}
+</STYLE>
+
+<div class="container ">
+<!-- Header -->
+
+  <div class="row header" style="margin-top: 5px">
+  <div class="alert alert-primary col-md-4" role="alert">
+  DOCUMENTO: # 4750
+  </div>
+    <div class="col-md-12">
+      <div class="input-group input-group-sm ">
+        <span class="input-group-addon">
+          42
+        </span>
+        <input type="text" class="form-control input-lg" value="Prod 1" disabled>
+        <span class="input-group-addon">
+          <input class= "lista" type="checkbox">
+        </span>
+      </div>
+      <div class="input-group input-group-sm">
+       <span class="input-group-addon">
+          452
+        </span>
+        <input type="text" class="form-control input-lg" value="Prod 2" disabled>
+        <span class="input-group-addon">
+          <input class= "lista" type="checkbox">
+        </span>
+      </div>
+      <div class="input-group input-group-sm">
+       <span class="input-group-addon">
+          122
+        </span>
+        <input type="text" class="form-control input-lg" value="Prod 3" disabled>
+        <span class="input-group-addon">
+          <input class= "lista" type="checkbox">
+        </span>
+      </div>
+      <div class="input-group input-group-sm">
+        <span class="input-group-addon">
+          782
+        </span>
+        <input type="text" class="form-control input-lg" value="Prod 4" disabled>
+        <span class="input-group-addon">
+          <input class= "lista" type="checkbox">
+        </span>
+      </div>
+      <div class="input-group input-group-sm">
+       <span class="input-group-addon">
+          824
+        </span>
+        <input type="text" class="form-control input-lg" value="Prod 5" disabled>
+        <span class="input-group-addon">
+          <input class= "lista" type="checkbox">
+        </span>
+      </div>
+    </div>
+    <div class="col-md-12">
+      <button type="submit" class="btn btn-primary">
+        Enviar a revisión
+      </button>
+    </div>
+  </div>
+</div>
+
