3ab0e8a42f3a05ffb3d7f32ba909ac64e4076998
Giovanni Blanco <kevin.cano.creation@gmail.com>
2018-01-26 22:32:41 -0600

    Guardar y mostrar datos
    Guardo y muestro los datos. Ojo: en estado aparece verde para activo y rojo para inactivo

--- app/Model/proveedores.php
+++ app/Model/proveedores.php
@@ -1,10 +1,61 @@
 <?php 
+require('Config/conexion.php');
 $send = htmlspecialchars($_SERVER['PHP_SELF']);
 $error = 0;
+$info = $cnx->query("SELECT * FROM proveedores");
+function tabla($x){
+	foreach ($x as $xx) {
+		$nombre = $xx['nombre'];
+		$direccion = $xx['direccion'];
+		$telefono = $xx['telefono'];
+		$credito = $xx['credito'];
+		$dias_credito = $xx['dias_credito'];
+		$nit = $xx['nit'];
+		$pais =  $xx['pais'];
+		$nota = $xx['nota'];
+		$estado = $xx['activo'];
+		$p_fecha = $xx['fechaop'];
+		$p_fecha = substr($p_fecha, 0, -9); $fecha_dia = substr($p_fecha, -2, 8); $fecha_mes = substr($p_fecha, 5, 2); $fecha_anio = substr($p_fecha, 0, 4);  //configuro la fecha d/m/a
+				switch ($fecha_mes) 
+				{ //el case es para cambiar de 01 a enero y todos los meses
+					case '01':$fecha_mes = 'ene.';break;case '02':$fecha_mes = 'feb.';break;case '03':$fecha_mes = 'mar.';break;case '04':$fecha_mes = 'abr.';break;case '05':$fecha_mes = 'mayo.';break;case '06':$fecha_mes = 'jun.';break;case '07':$fecha_mes = 'jul.';break;case '08':$fecha_mes = 'ago.';break;case '09':$fecha_mes = 'sep.';break;case '10':$fecha_mes = 'oct.';break;case '11':$fecha_mes = 'nov.';break;case '12':$fecha_mes = 'dic.';break;default:break;
+				}//configuracion para que la fecha quede bien
+				$p_fecha = $fecha_dia.' '.$fecha_mes.' '.$fecha_anio;
+		if ($estado == 1) {
+			$estado = "<div class='activo'></div>";
+		}else{
+			$estado = "<div class='noactivo'></div>";
+		}
+		echo "
+			<tr>
+			<th>$nombre</th>
+			<th>$direccion</th>
+			<th>$telefono</th>
+			<th>$nit	</th>
+			<th>$credito</th>
+			<th>$dias_credito</th>
+			<th>$pais	</th>
+			<th>$nota	</th>
+			<th>$estado	</th>
+			<th>$p_fecha	</th>
+			<th><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#viewproveedor'>Editar</button></th>
+			</tr>
+		";
+
+	}
+}
+
+
+
+
+
+
+
+
+
 	if (isset($_POST['new'])) 
 	{
-		require('Config/conexion.php');
-		function limpiar($x){$x = trim($x);$x = htmlspecialchars($x);$x = stripcslashes($x);}
+		function limpiar($x){$x = trim($x);$x = htmlspecialchars($x);$x = stripcslashes($x);return $x;}
 		$nombre = $_POST['nombre']; $nombre = limpiar($nombre);
 		$direccion = $_POST['direccion']; $direccion = limpiar($direccion);
 		$telefono = $_POST['telefono']; $telefono = limpiar($telefono);
@@ -14,13 +65,15 @@ $error = 0;
 		$pais = $_POST['pais']; if ($pais == 1) {$error = 1;}//si dejan sin seleccionar el pais da error 1
 		$nota = $_POST['nota']; $nota = limpiar($nota);
 		$estado = 1;
-		if ($error == 0) {
-			$guardar = $cnx->query("INSERT INTO `proveedores` (`idprov`, `nombre`, `direccion`, `telefono`, `credito`, `dias_credito`, `nit`, `pais`, `nota`, `activo`, `fechaop`) VALUES (NULL, '$nombre', '$direccion', '$telefono', '$credito', '$dias_credito', '$nit', '$pais', '$nota', '1'");
-			if ($guardar) {
-			header("Location: principal.php");
-		}else{
-			echo "Error al guardar el dato.";
-		}
+		if ($error == 0) 
+		{
+			$guardar = $cnx->query("INSERT INTO `proveedores` (`idprov`, `nombre`, `direccion`, `telefono`, `credito`, `dias_credito`, `nit`, `pais`, `nota`, `activo`) VALUES (NULL, '$nombre', '$direccion', '$telefono', '$credito', '$dias_credito', '$nit', '$pais', '$nota', '$estado');");
+			if ($guardar == true) 
+			{
+				header("Location: principal.php");
+			}else{
+				echo "Error al guardar el dato.";
+			}
 		}if ($error == 1) {
 			echo "Error con los datos.";
 		}
--- app/manejo_prov.php
+++ app/manejo_prov.php
@@ -1,4 +1,23 @@
 <?php require_once('Model/proveedores.php'); ?>
+<style type="text/css">
+  .activo {
+     width: 1rem;
+     height: 1rem;
+     -moz-border-radius: 50%;
+     -webkit-border-radius: 50%;
+     border-radius: 50%;
+     background: #5cb85c;
+}
+ .noactivo {
+     width: 1rem;
+     height: 1rem;
+     -moz-border-radius: 50%;
+     -webkit-border-radius: 50%;
+     border-radius: 50%;
+     background: red;
+}
+</style>
+
 <button type="button" class="btn btn-primary float-right mb-2" data-toggle="modal" data-target="#addproveedor">
   Agregar Proveedor
 </button>
@@ -8,64 +27,36 @@
                   <th>Nombre</th>
                   <th>Direccion</th>
                   <th>Telefono</th>
+                  <th>Nit</th>
                   <th>Credito</th>
                   <th>Días de Credito</th>
                   <th>Pais</th>
                   <th>Nota</th>
                   <th>Estado</th>
+                  <th>Fecha</th>
                   <th>Opciones</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                  <th>1</th>
-                  <th>1</th>
-                  <th>1</th>
-                  <th>1</th>
-                  <th>1</th>
-                  <th>1</th>
-                  <th>1</th>
-                  <th>1</th>
-                  <th>1</th>
-                </tr>
+                 <?php tabla($info); ?>
                 </tbody>
                 <tfoot>
                 <tr>
                   <th>Nombre</th>
                   <th>Direccion</th>
                   <th>Telefono</th>
+                  <th>Nit</th>
                   <th>Credito</th>
                   <th>Días de Credito</th>
                   <th>Pais</th>
                   <th>Nota</th>
                   <th>Estado</th>
+                  <th>Fecha</th>
                   <th>Opciones</th>
                   </tr>
                 </tfoot>
               </table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- Modal -->
+<!-- Add -->
 <div class="modal fade" id="addproveedor" tabindex="-1" role="dialog" aria-labelledby="addproveedorLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -77,41 +68,93 @@
       </div>
       <div class="modal-body">
         <form action="<?php echo $send; ?>" method="POST" class="form">
-			<div class="form-group">
-  			  <label for="nombre">Nombre</label>
-  			  <input type="text" class="form-control" required="true" name="nombre" id="nombre" placeholder="Nombre">
-  			</div>
-  			<div class="form-group">
-  			  <label for="direccion">Direccion</label>
-  			  <input type="text" class="form-control" required="true" name="direccion" id="direccion" placeholder="Direccion">
-  			</div>
-  			<div class="form-group">
-  			  <label for="telefono">Telefono</label>
-  			  <input type="number" class="form-control" required="true" name="telefono" id="telefono" placeholder="Telefono">
-  			</div>
-  			<div class="form-group">
-  			  <label for="credito">Credito</label>
-  			  <input type="text" class="form-control" required="true" name="credito" id="credito" placeholder="Credito">
-  			</div>
-  			<div class="form-group">
-  			  <label for="dias_credito">Dias del Credito</label>
-  			  <input type="number" class="form-control" required="true" name="dias_credito" id="dias_credito" placeholder="Dias del credito">
-  			</div>
-  			<div class="form-group">
-  			  <label for="nit">Nit</label>
-  			  <input type="text" class="form-control" required="true" name="nit" id="nit" placeholder="Nit">
-  			</div>
-  			<div class="form-group">
-  			  <?php require_once('paises.html'); ?>
-  			</div>
-  			<div class="form-group">
-  			  <label for="nota">Nota</label>
-  			  <textarea class="form-control" name="nota" id="nota" placeholder="Nota"></textarea>
-  			</div>
+      <div class="form-group">
+          <label for="nombre">Nombre</label>
+          <input type="text" class="form-control" required="true" name="nombre" id="nombre" placeholder="Nombre">
+        </div>
+        <div class="form-group">
+          <label for="direccion">Direccion</label>
+          <input type="text" class="form-control" required="true" name="direccion" id="direccion" placeholder="Direccion">
+        </div>
+        <div class="form-group">
+          <label for="telefono">Telefono</label>
+          <input type="number" class="form-control" required="true" name="telefono" id="telefono" placeholder="Telefono">
+        </div>
+        <div class="form-group">
+          <label for="credito">Credito</label>
+          <input type="text" class="form-control" required="true" name="credito" id="credito" placeholder="Credito">
+        </div>
+        <div class="form-group">
+          <label for="dias_credito">Dias del Credito</label>
+          <input type="number" class="form-control" required="true" name="dias_credito" id="dias_credito" placeholder="Dias del credito">
+        </div>
+        <div class="form-group">
+          <label for="nit">Nit</label>
+          <input type="text" class="form-control" required="true" name="nit" id="nit" placeholder="Nit">
+        </div>
+        <div class="form-group">
+          <?php require_once('paises.html'); ?>
+        </div>
+        <div class="form-group">
+          <label for="nota">Nota</label>
+          <textarea class="form-control" name="nota" id="nota" placeholder="Nota"></textarea>
+        </div>
         <div class="modal-footer">
         <button type="submit" name="new" class="btn btn-success">Guardar</button>
       </div>
-		</form>
+    </form>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<!-- view -->
+<div class="modal fade" id="viewproveedor" tabindex="-1" role="dialog" aria-labelledby="viewproveedorLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="viewproveedorLabel">Agregar Proveedor</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?php echo $send; ?>" method="POST" class="form">
+      <div class="form-group">
+          <label for="nombre">Nombre</label>
+          <input type="text" class="form-control" required="true" name="nombre" id="nombre" placeholder="Nombre">
+        </div>
+        <div class="form-group">
+          <label for="direccion">Direccion</label>
+          <input type="text" class="form-control" required="true" name="direccion" id="direccion" placeholder="Direccion">
+        </div>
+        <div class="form-group">
+          <label for="telefono">Telefono</label>
+          <input type="number" class="form-control" required="true" name="telefono" id="telefono" placeholder="Telefono">
+        </div>
+        <div class="form-group">
+          <label for="credito">Credito</label>
+          <input type="text" class="form-control" required="true" name="credito" id="credito" placeholder="Credito">
+        </div>
+        <div class="form-group">
+          <label for="dias_credito">Dias del Credito</label>
+          <input type="number" class="form-control" required="true" name="dias_credito" id="dias_credito" placeholder="Dias del credito">
+        </div>
+        <div class="form-group">
+          <label for="nit">Nit</label>
+          <input type="text" class="form-control" required="true" name="nit" id="nit" placeholder="Nit">
+        </div>
+        <div class="form-group">
+          <?php require_once('paises.html'); ?>
+        </div>
+        <div class="form-group">
+          <label for="nota">Nota</label>
+          <textarea class="form-control" name="nota" id="nota" placeholder="Nota"></textarea>
+        </div>
+        <div class="modal-footer">
+        <button type="submit" name="new" class="btn btn-success">Guardar</button>
+      </div>
+    </form>
       </div>
       
     </div>
