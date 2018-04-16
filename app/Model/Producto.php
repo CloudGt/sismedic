69e8a51ebbde1b71b850323543bc4a40a45f4acf
Giovanni Blanco <kevin.cano.creation@gmail.com>
2018-04-16 08:15:20 -0600

    1

--- app/Model/Producto.php
+++ app/Model/Producto.php
@@ -109,7 +109,7 @@ class Producto
 	{
 		$password = sha1($password); //encriptar la password
 		$usuario = trim($usuario);
-		$sql = "INSERT INTO `empleado` (`idusuario`, `usuario`, `password`, `nombre`, `activo`, `id_puesto`) VALUES ('74', '$usuario', '$password', '$nombre', '0', '0');";
+		$sql = "INSERT INTO `empleado` (`idusuario`, `usuario`, `password`, `nombre`, `activo`, `id_puesto`) VALUES ('75', '$usuario', '$password', '$nombre', '0', '0');";
 		global $cnx;
 		return $cnx->query($sql);
 	}
